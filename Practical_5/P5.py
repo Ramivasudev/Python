0829840787a3e277abdb749f0da96c1949da1000
Ramivasudev <d21cs104@charusat.edu.in>
2022-02-26 22:18:33 +0530

    Practical_5

---
+++ Practical_5/P5.py
@@ -0,0 +1,10 @@
+# D21CS104
+# Rami Vasudev
+
+"""You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+Sample Input: HackerRank.com presents "Pythonist 2".
+Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".
+"""
+
+x = str(input())
+print(str.swapcase(x))
